9dcf6111102e4b1d51001049e3ca0513ee7d2953
Maer120312 <maesparzarodriguez@ucgdl.edu.mx>
2024-09-14 14:23:22 -0600

    initial project

---
+++ Programacion_a_objetos/src/main/java/com/mycompany/programacion_a_objetos/Programacion_a_objetos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacion_a_objetos;
+
+/**
+ *
+ * @author miguel.esparza
+ */
+public class Programacion_a_objetos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
