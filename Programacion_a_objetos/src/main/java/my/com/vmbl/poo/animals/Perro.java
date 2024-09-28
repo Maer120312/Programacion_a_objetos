b753ad0b20b8c7470c6ca2318f70b76b3a2f7536
Maer120312 <maesparzarodriguez@ucgdl.edu.mx>
2024-09-28 12:28:35 -0600

    añadi mas animales y figura

---
+++ Programacion_a_objetos/src/main/java/my/com/vmbl/poo/animals/Perro.java
@@ -0,0 +1,10 @@
+package my.com.vmbl.poo.animals;
+
+public class Perro extends Animal {
+    @Override
+     public void comer(){
+        System.out.println("Perro");
+    }
+}
+    
+
---
+++ Programacion_a_objetos/src/main/java/my/com/vmbl/poo/animals/Tiburon.java
@@ -0,0 +1,9 @@
+package my.com.vmbl.poo.animals;
+
+
+public class Tiburon extends Animal {
+    @Override
+    public void comer(){
+        System.out.println("Tiburon");
+    }
+}
