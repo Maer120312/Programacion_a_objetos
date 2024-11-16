b16ddc24d75050b38d954a985537e745b2cf5dd6
Maer120312 <maesparzarodriguez@ucgdl.edu.mx>
2024-11-16 09:04:24 -0600

    Update Programacion_a_objetos.java

--- Programacion_a_objetos/src/main/java/my/com/maer/poo/programacion_a_objetos/Programacion_a_objetos.java
+++ Programacion_a_objetos/src/main/java/my/com/maer/poo/programacion_a_objetos/Programacion_a_objetos.java
@@ -7,6 +7,7 @@ import my.com.maer.poo.polimorfismo.Estudiante;
 import my.com.maer.poo.polimorfismo.Profesor;
 import my.com.maer.poo.patrones_de_diseno.Builder.ConstructorPersona;
 import my.com.maer.poo.patrones_de_diseno.Builder.Persona;
+import my.com.maer.poo.patrones_de_diseno.PuertoSerie;
 import my.com.maer.poo.patrones_diseño_decorator.Decorator;
 import my.com.maer.poo.patrones_diseño_decorator.Primo;
 import my.com.maer.poo.patrones_diseño_decorator.PrimoOptimizado;
@@ -25,6 +26,90 @@ public class Programacion_a_objetos {
 
     public static void main(String[] args) 
     {
+        
+                Manager manager = new Manager();
+        
+        Subscriptor usuario1 = new Usuario("Miguel");
+        Subscriptor usuario2 = new Usuario("Victor");
+        
+        manager.agregarSubscriptor(usuario1);
+        manager.agregarSubscriptor(usuario2);
+        
+        manager.publicarNewsletter("Primera newsletter");
+        
+        Subscriptor usuario3 = new Usuario("Pedro");
+        manager.agregarSubscriptor(usuario3);
+        
+        manager.publicarNewsletter("Segunda newsletter");
+        
+        manager.eliminarSubscriptor(usuario2);
+        
+        manager.publicarNewsletter("Ultima newsletter");
+        
+        System.exit(0);
+        
+//        Tarea t1 = new Tarea1();
+//        Decorator d1 = new Decorator(t1);
+//        d1.ejecutar();
+//        
+//        Tarea t2 = new Tarea2();
+//        Decorator d2 = new Decorator(t2);
+//        d2.ejecutar();
+//        
+//        Tarea t3 = new Tarea3();
+//        Decorator d3 = new Decorator(t3);
+//        d3.ejecutar();
+//        
+//        Tarea t4 = new Tarea4();
+//        Decorator d4 = new Decorator(t4);
+//        d4.ejecutar();
+       
+        Tarea t1 = new Primo(999983); // 29989 999983
+        Decorator d1 = new Decorator(t1);
+        d1.ejecutar();
+        
+        Tarea t2 = new PrimoOptimizado(999983); // 29989 999983
+        Decorator d2 = new Decorator(t2);
+        d2.ejecutar();
+        
+        System.exit(0);
+        
+        ConstructorPersona cp = new ConstructorPersona();
+        cp.setNombre("Pedro");
+        cp.setApellido("Valenzuela");
+        cp.setOcupacion("Profesor");
+        
+        Persona persona = cp.construir();
+        
+        System.out.println(persona);
+        
+        System.exit(0);
+        
+        PuertoSerie ps1 = PuertoSerie.obtenerInstancia();
+        //PuertoSerie ps2 = new PuertoSerie();
+        PuertoSerie ps2 = PuertoSerie.obtenerInstancia();
+        PuertoSerie ps3 = PuertoSerie.obtenerInstancia();
+        
+        System.out.println(ps1);
+        System.out.println(ps2);
+        System.out.println(ps3);
+        
+        System.exit(0);
+        
+        Static.funcionEstatica();
+        
+        Static s = new Static();
+        s.funcionNoEstatica();
+        
+        
+        Static s2 = new Static();
+        Static s3 = new Static();
+        Static s4 = new Static();
+        Static s5 = new Static();
+        
+        System.exit(0);
+        
+       
         
         Estudiante e1 = new Estudiante("Pedro", "luis@usg", 3);
         Estudiante e2 = new Estudiante("miguel Esparza", "luis@usg", 3 );
